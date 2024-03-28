cead9c9089e487cd29d2f15a525ac3fc9116ea4d
Nihir Zala <zalanihir8866@gmail.com>
2024-03-28 11:21:02 +0530

    added the sub menus like products and all

---
+++ pages/products/[id].vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Product Id Page</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae, optio sed? Temporibus dolore dignissimos corporis magnam non adipisci exercitationem deleniti ratione facere odit, minus mollitia nesciunt praesentium assumenda est a.</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+h2{
+    margin-bottom: 30px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
